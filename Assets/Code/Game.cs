d73b617266fe88d428a202fda1b369a2c0017663
Thotsaphorn <a0867900352@gmail.com>
2026-03-06 06:08:20 +0700

    Shop

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class Game : MonoBehaviour
 {
@@ -10,13 +14,27 @@ public class Game : MonoBehaviour
     public float scoreIncreasedPerSecond;
     public float x;
 
+    //SHOP
+    public int shop1prize;
+    public TextMeshProUGUI shop1text;
+    public int shop2prize;
+    public TextMeshProUGUI shop2text;
+
+    //AMOUNT
+    public TextMeshProUGUI amount1Text;
+    public int amount1;
+    public float amount1Profit;
+    public TextMeshProUGUI amount2Text;
+    public int amount2;
+    public float amount2Profit;
+
     // Use this for initialization
     void Start () {
 
         //CLICKER
         currentScore =0;
         hitPower =1;
-        //scoreIncreasedPerSecond =1;
+        scoreIncreasedPerSecond =1;
         x =0f;
 
     }
@@ -26,7 +44,16 @@ public class Game : MonoBehaviour
 
         //CLICKER
         scoreText.text = "Heart Score: " + (int)currentScore + " Point";
-        //currentScore += scoreIncreasedPerSecond * Time.deltaTime;
+        scoreIncreasedPerSecond = x * Time.deltaTime;
+        currentScore = currentScore + scoreIncreasedPerSecond;
+
+        //SHOP
+        shop1text.text = "Tier 1: " + shop1prize + " $";
+        shop2text.text = "Tier 2: " + shop2prize + " $";
+
+        //AMOUNT
+        amount1Text.text = "Tier 1: " + amount1 + " arts $: " + amount1Profit + "/s";
+        amount2Text.text = "Tier 2: " + amount2 + " arts $: " + amount2Profit + "/s";
 
     }
 
@@ -35,5 +62,30 @@ public class Game : MonoBehaviour
 
         currentScore += hitPower;
 
+    }
+
+    //SHOP
+    public void Shop1()
+    {
+        if (currentScore >= shop1prize)
+        {
+            currentScore -= shop1prize;
+            amount1 += 1;
+            amount1Profit += 1;
+            x += 1;
+            shop1prize += 25;
+        }
+    }
+
+    public void Shop2()
+    {
+        if (currentScore >= shop2prize)
+        {
+            currentScore -= shop2prize;
+            amount2 += 1;
+            amount2Profit += 5;
+            x += 5;
+            shop2prize += 125;
+        }
     }
 }
